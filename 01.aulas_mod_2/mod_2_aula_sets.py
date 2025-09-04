fc2c8fed5298dce1ccf484582bc455dafdfe8ef0
Brendo T. LAB <=>
2025-09-04 20:52:30 -0300

    UPDADE aula 2 - mod 2

--- 01.aulas_mod_2/mod_2_aula_sets.py
+++ 01.aulas_mod_2/mod_2_aula_sets.py
@@ -1,4 +1,4 @@
-#SETS E MANIPULAÇÃO DE ARQUIVOS
+#SETS, MANIPULAÇÃO DE ARQUIVOS, TRATAMENTO DE DADOS
 
 with open ("arquivo_aula-2.txt","w") as arquivo_aula2:
     #with --> "Com esse arquivo, faça algo." Ele fecha o arquivo ao final do bloco.
@@ -12,11 +12,11 @@ with open ("arquivo_aula-2.txt","w") as arquivo_aula2:
         Adicionando linha 5""")
 
 
-"""with open ("arquivo_aula-2.txt","r") as arquivo_aula2:
-   print(arquivo_aula2.read())"""
+# with open ("arquivo_aula-2.txt","r") as arquivo_aula2:
+#    print(arquivo_aula2.read())"""
 
-with open ("arquivo_aula-2.txt","r") as arquivo_aula2:
-    print(arquivo_aula2.readline())
+# with open ("arquivo_aula-2.txt","r") as arquivo_aula2:
+#     print(arquivo_aula2.readline())"""
 
 #with open ("arquivo_aula-2.txt","r") as arquivo_aula2:
 #    print(arquivo_aula2.readlines())

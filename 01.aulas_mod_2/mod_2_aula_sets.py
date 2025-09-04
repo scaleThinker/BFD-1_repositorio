c9ba2a0030f139cf781227f6841d01f82c5d4ee2
Brendo T. LAB <=>
2025-09-04 19:56:39 -0300

    Adiciona exercicio de manipulação de arquivos.

--- 01.aulas_mod_2/mod_2_aula_sets.py
+++ 01.aulas_mod_2/mod_2_aula_sets.py
@@ -1,55 +1,22 @@
-#MANIPULAÇÃO DE STRINGS
+#SETS E MANIPULAÇÃO DE ARQUIVOS
 
-texto = "    Curso Back-EndPython    "
+with open ("arquivo_aula-2.txt","w") as arquivo_aula2:
+    #with --> "Com esse arquivo, faça algo." Ele fecha o arquivo ao final do bloco.
+    #open --> abre um arquivo (nome do arquivo, o que fazer com o arquivo)
+    #as --> apelidar coisas. Interprete o arquivo como ...
+    arquivo_aula2.write(
+        """Testando a função .write()
+        Adicionando linha 2
+        Adicionando linha 3
+        Adicionando linha 4
+        Adicionando linha 5""")
 
-print("#MAIÚSCULAS E MINÚSCULAS")
-print ("Lower =\n",texto.lower()) #Converte tudo para minúsculas
-print ("Capitalize =\n",texto.capitalize()) #Deixa a primeira letra maiuscula
-print ("Upper =\n",texto.upper()) #Converte tudo para maiúscula
-print ("Title =\n",texto.title()) #Cada primeira letra maiúscula
 
-print("#LIMPEZA E TRATAMENTO")
-print ("Strip =\n",texto.strip("Curso ")) #Remove espaços em branco (no meio não funciona)(ou caractere especificado).
-print ("Lstrip =\n",texto.lstrip()) #Remove espaços do início (esquerda)
-print ("Rstrip =\n",texto.rstrip()) #Remove espaços do fim (direita)
-print ("Replace =\n",texto.replace("Curso","Curso BFD")) #Substitui todas as aparições de uma substring pela nova
+"""with open ("arquivo_aula-2.txt","r") as arquivo_aula2:
+   print(arquivo_aula2.read())"""
 
-print("#BUSCANDO STRINGS E SUBSTRINGS")
-print("In =\n","Curso" in texto) #verifica se a substring existe numa string.
-print ("Find =\n",texto.find("so")) #retorna o índice da primeira ocorrência da substring procurada.
-print ("Index =\n",texto.index("Python")) #igual o .find() mas retorna erro se não encontrar.
-print ("Startswith =\n",texto.startswith("Curso")) #Verifica se a string começa com o texto especificado.
-print ("Endswith =\n",texto.endswith("Python    ")) #Verifica se a string termina com o texto especificado.
+with open ("arquivo_aula-2.txt","r") as arquivo_aula2:
+    print(arquivo_aula2.readline())
 
-print("#DIVIDIR E JUNTAR")
-novotexto = texto.split("-") #Divide uma string em uma lista de substrings a partir do caractere escolhido como separador, se vazio, divide a partir dos espaços.
-print ("Split =\n",novotexto)
-texto_novo = "".join(novotexto) #oncatena os elementos de uma lista em uma única string (oposto do split). A sintaxe deste método é um pouco diferente do padrão, ele deve ser aplicado ao separador e não a lista.
-print ("Join =\n",texto_novo)
-
-print("#VALIDAÇÕES")
-print ("Isdigit =\n",texto.isdigit()) #Retorna True se todos os caracteres forem números.
-print ("Isalpha =\n",texto.isalpha()) #Retorna True se todos os caracteres são letras do alfabeto.
-print ("Isalnum =\n",texto.isalnum()) #Retorna True se todos os caracteres são alfanuméricos (letras ou números).
-print ("Isspace =\n",texto.isspace()) #Retorna True se todos os caracteres são espaço em branco ou quebra de linha.
-
-print("#FORMATAÇÃO")
-curso = "Python"
-
-dia = "terças e quintas"
-
-num_dec = 0.22556633
-print(f"""F-STRING =
-O curso de {curso} acontece as {dia}, {num_dec:.2f}""")
-
-print("#ENCODING")
-
-texto = "Olá, fração, decoding"
-
-b_utf = texto.encode('utf-8')
-
-print(b_utf)
-
-texto_decodificado = b_utf.decode('utf-8')
-
-print(texto_decodificado)
+#with open ("arquivo_aula-2.txt","r") as arquivo_aula2:
+#    print(arquivo_aula2.readlines())

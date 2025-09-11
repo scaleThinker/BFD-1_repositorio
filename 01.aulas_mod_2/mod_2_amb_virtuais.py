fb52bd25a4fb23842c1fb601360820d8fb7391c2
Brendo T. LAB <=>
2025-09-11 20:48:09 -0300

    Adiciona Ambientes Virtuais e Orientação a OBJ

--- 01.aulas_mod_2/mod_2_amb_virtuais.py
+++ 01.aulas_mod_2/mod_2_amb_virtuais.py
@@ -1,4 +1,7 @@
 #ao instalar bibliotecas com pip, podemos ter versoes diferentes
 #pip freeze "nome de um arquivo" - geralmente chamado de requirements.txt
 # dentro do terminal que esta o requirements.txt usando pip install requirements.txt quem estiver usando pode instalar diretamente as bibliotecas daquele projeto
+# para criar um ambiente virtual: python -m venv venv
+# ele criará uma pasta com o python vazio + pip
+# o activate está dentro da pasta venv > Scripts
 
---
+++ 01.aulas_mod_2/mod_2_orientação_obj.py
@@ -0,0 +1,5 @@
+class Car:
+    def __init__(self, year, make, model):
+        self.year = year
+        self.make = make
+        self.model = model

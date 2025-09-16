e2599e719271eba74708f1272eb0de6ae02f0859
Brendo T. LAB <=>
2025-09-16 20:56:43 -0300

    Adiciona exercicio carro

--- 01.aulas_mod_2/mod_2_orientação_obj.py
+++ 01.aulas_mod_2/mod_2_orientação_obj.py
@@ -1,11 +1,4 @@
 """
-class Car:
-    def __init__(self, year, make, model):
-        self.year = year
-        self.make = make
-        self.model = model
-"""
-
 class Aluno: #primeira letra maiuscula
 
     def __init__(self,name,sexo,idade,nota1b,nota2b): #a primeira def sempre __init__
@@ -28,3 +21,51 @@ aluno2=Aluno(name="Maria Joaquina da Motta",sexo="F", idade=13, nota1b=8.2, nota
 print(f"O nome do(a) aluno(a) é {aluno1.name}")
 
 print(aluno2.medianotas())
+"""
+
+class Car:
+    def __init__(self, year, status, avgas):
+        self.year=year
+        self.status=status
+        self.avgas= avgas 
+
+    def ver_status(self):
+        if self.status == True:
+            return f"O carro está ligado."
+        else:
+            return f"O carro está desligado."
+    
+    def ver_agas(self):
+        if self.avgas >= 0.9:
+            return f"Gasolina em status seguro."
+        elif self.avgas < 0.9 and self.avgas > 0.5:
+            return f"Abasteça em breve."
+        elif self.avgas < 0.5:
+            return f"Abasteça!"
+    
+    def ver_ipva(self):
+        if self.year >= 2015:
+            return f"O carro deve pagar IPVA."
+        elif self.year < 2015:
+            return f"O carro não precisa pagar IPVA."
+    
+    def rest_ipva(self):
+        if self.year < 2015:
+            return f"O carro não precisa pagar IPVA."
+        elif self.year > 2015:
+            t_rest_ipva = 10 - (2025 - self.year)
+            return f"Seu carro irá parar de pagar IPVA em {t_rest_ipva} anos."
+    def car_off (self):
+        status = self.status = False
+        return status
+        
+
+car1 = Car (year = 1990, status = True, avgas = .30)
+car2 = Car (year = 2019, status = False, avgas = .77)
+car3 = Car (year = 2024, status = True, avgas = .51)
+
+print(car1.ver_agas())
+print(car3.car_off())
+print(car2.ver_ipva())
+print(car1.ver_status())
+print(car2.rest_ipva())

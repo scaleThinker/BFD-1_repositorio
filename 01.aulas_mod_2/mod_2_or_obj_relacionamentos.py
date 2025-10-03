a74d6ecf247d1b3bfc0125709303a321fe558a00
Brendo <qq@gmail.com>
2025-10-02 21:06:54 -0300

    Adiciona exercicio agregacao

--- 01.aulas_mod_2/mod_2_or_obj_relacionamentos.py
+++ 01.aulas_mod_2/mod_2_or_obj_relacionamentos.py
@@ -4,3 +4,35 @@
 - Herança;
 - Dependência.
 """
+class Livro:
+    def __init__(self, autor):
+        self.autor = autor
+        self.cap = []
+
+    def adicionar_cap(self,capitulo):
+        self.cap.append(capitulo)
+        print(f"'{capitulo.texto}' foi adicionado ao livro.")
+
+    def exibircap(self):
+        for capitulo in self.cap:
+            capitulo.exibir_txt()
+            
+
+class Capitulo:
+    def __init__(self, texto):
+        self.texto = texto
+
+    def exibir_txt(self):
+        print (f"{self.texto}")
+
+
+livr = Livro("Mauricio de Abreu")
+
+cap = Capitulo("bla")
+cap2 = Capitulo("XY")
+livr.adicionar_cap(cap2)
+livr.adicionar_cap(cap)
+livr.exibircap()
+
+
+
